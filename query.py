f38a8038041590afe8f42c0091b102b2edfcf6a9
pfontaine <pfontaine@etu.uqac.ca>
2025-01-29 18:28:47 -0500

    tried to improve the prompt

--- query.py
+++ query.py
@@ -4,20 +4,21 @@ from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
 from langchain_core.prompts import ChatPromptTemplate
 
-from scrapping import vector_store
-from setup import llm
+from setup import llm, vector_store
 
 
 # Define prompt for question-answering
 # prompt = hub.pull("rlm/rag-prompt")
 
+# todo: améliorer ce prompt
+# todo: utiliser https://python.langchain.com/docs/how_to/chatbots_memory/ pour la mémoire des questions précédentes
 prompt = ChatPromptTemplate.from_messages([
     ("system", "tu es un assistant IA prêt à répondre à toutes mes questions concernant le manuel de gestion de l'UQAC. "
                "Utilises les éléments de contexte suivants pour répondre à la question. Si tu ne connais pas la réponse, dis-le simplement."
-               "Essaie de répondre de manière concise et précise."),
+               "Essaie de répondre de manière précise en reprenant qusi mot pour mot les éléments de contexte lorsque c'est pertinent."),
     ("human", "{question}"),
-    ("system", "Voici les éléments de contexte our répondre à la question : {context}"
-               "Ainsi aue les messages précédents : {history}"),
+    ("system", "Voici les éléments de contexte pour répondre à la question : {context}"
+               "Ainsi que les messages précédents : {history}"),
 ])
 
 # Define state for application
@@ -30,7 +31,7 @@ class State(TypedDict):
 
 # Define application steps
 def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
+    retrieved_docs = vector_store.similarity_search(state["question"], k=4)
     return {"context": retrieved_docs}
 
 
@@ -59,5 +60,3 @@ def update_history(state: State):
 graph_builder = StateGraph(State).add_sequence([retrieve, generate, update_history])
 graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
-
-print("graph compiled")
