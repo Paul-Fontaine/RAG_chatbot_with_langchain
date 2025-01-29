3fa81d24c74a849a04925ffa7bf0fe8aac665dad
pfontaine <pfontaine@etu.uqac.ca>
2025-01-29 18:28:14 -0500

    create a function to ask question and return the answer

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from query import graph
 
-# question = str(input("Enter your question: "))
-# if question == "0":
-question = "Quels sont les objectifs de la politique de développement durable de l'UQAC ?"
-print("question:", question)
 
-print("answering ...")
-response = graph.invoke({"question": question})
-answer = response["answer"]
-sources = set([doc.metadata['source'] for doc in response['context'] if doc.metadata['source'] is not None])
-print("réponse :\n", answer)
-print("\nsources : \n", "\n".join(sources), "\n")
+def ask_question(question: str):
+    print("answering ...")
+    response = graph.invoke({"question": question})
+    answer = response["answer"]
+    sources = set([doc.metadata['source'] for doc in response['context'] if doc.metadata['source'] is not None])
+    reponse = (f"réponse :\n"
+               f"{answer}\n"
+               f"sources : \n"
+               f"{"\n".join(sources)}\n")
+    print(reponse)
 
-question2 = "À qui s'applique cette politique et qui est le responsable de son application ?"
-print("question:", question2)
-response2 = graph.invoke({"question": question2})
-answer2 = response2["answer"]
-sources2 = set([doc.metadata['source'] for doc in response2['context'] if doc.metadata['source'] is not None])
-print("réponse :\n", answer2)
-print("\nsources : \n", "\n".join(sources2), "\n")
+while True:
+    # question = str(input("question :\n"))
+    question = "Quelle est la langue de travail de l'UQAC ?"
+    if question == "exit":
+        break
+    ask_question(question)
 
-question3 = "Reformule ta réponse précédente."
-print("question:", question3)
-response3 = graph.invoke({"question": question3})
-answer3 = response3["answer"]
-sources3 = set([doc.metadata['source'] for doc in response3['context'] if doc.metadata['source'] is not None])
-print("réponse :\n", answer3)
-print("\nsources : \n", "\n".join(sources3), "\n")
+# Quels sont les objectifs de la politique de développement durable de l'UQAC ?
+# Qui est responsable de son application ?
+# Quelle est la définition du développement durable  dans le manuel de gestion de l'uqac?
+# Quelles sont les principales fonctions du Comité de mise en œuvre du développement durable ?
